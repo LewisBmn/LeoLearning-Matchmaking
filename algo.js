a1d7cc7871d927e279fe23a7df5f5b80ea3cffd5
GaelleRgd <77722509+GaelleRgd@users.noreply.github.com>
2021-03-24 18:39:35 +0100

    Définition des variables

--- algo.js
+++ algo.js
@@ -1,1 +1,25 @@
+/*Types des variables d'éentrée :
+ - Liste des cours proposés dans la semaine 
+        ID Tuteur
+        Matière préférée
+        Date
+        Heure
+        Matières possibles pour ce cours (pas nécéssaire si on a carrément une instance du tuteur)
+        Nombre maximal de participants 
+ - Liste des demandes de cours faites pour toute la semaine
+        Date
+        Heure
+        ID Tutoré
+        Matière demandée 
+        priorité (ou nb de demandes + nb d'acceptations + nb d'absences)
 
+
+Types des variables de sortie : 
+ - Liste des cours validés 
+        Date
+        Heure
+        ID Tuteur
+        Liste des ID des tutorés 
+        Matrière
+ - Nb de priorités satisfaites (ou poircentage de remplissage)
+*/
